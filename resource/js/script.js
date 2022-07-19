4476ee2624516dc8367a4246cc6d4c42761a7eb8
suarnaya <kedusashram99@gmail.com>
2022-07-19 14:12:25 +0800

    add variable

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,2 +1,7 @@
+//automatic date
 const y = new Date();
 document.getElementById('date').innerHTML = y.getFullYear();
+
+//disable right click
+const body = document.querySelector('body');
+body.setAttribute('oncontextmenu', 'return false');
