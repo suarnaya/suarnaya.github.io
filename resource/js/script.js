7ca5541b553a06a4970c98911f1eaf0298b640fa
suarnaya <kedusashram99@gmail.com>
2022-08-07 14:36:53 +0800

    .

--- resource/js/script.js
+++ resource/js/script.js
@@ -5,3 +5,8 @@ document.getElementById('date').innerHTML = y.getFullYear();
 //disable right click
 const body = document.querySelector('body');
 body.setAttribute('oncontextmenu', 'return false');
+
+// animate logo name
+const logo = document.querySelector('.yellow-box');
+const huruf = [...logo.textContent].map((h) => `<span>${h}</span>`).join('');
+logo.innerHTML = huruf;
